716e2bb827de212febfe029b6096695c30f302d0
Your Name <princesslorna2000@gmail.com>
2024-09-14 19:01:57 +0300

    new

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,44 @@
 class Song:
-    pass
+    count = 0  # Class attribute to count songs
+    genres = []  # Class attribute to store unique genres
+    artists = []  # Class attribute to store unique artists
+    genre_count = {}  # Class attribute to store genre counts
+    artist_count = {}  # Class attribute to store artist counts
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(self.genre)  # Pass genre directly
+        self.add_to_artists(self.artist)  # Pass artist directly
+        self.add_to_genre_count(self.genre)
+        self.add_to_artist_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
